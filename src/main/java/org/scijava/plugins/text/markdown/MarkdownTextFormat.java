94968c25c93a8fab6ed58fc608ea0889491e5924
Jan Eglinger <jan.eglinger@fmi.ch>
2018-09-11 16:48:53 +0200

    Switch from markdownj to flexmark

--- src/main/java/org/scijava/plugins/text/markdown/MarkdownTextFormat.java
+++ src/main/java/org/scijava/plugins/text/markdown/MarkdownTextFormat.java
@@ -30,18 +30,18 @@
 
 package org.scijava.plugins.text.markdown;
 
-import com.petebevin.markdown.MarkdownProcessor;
-
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
 import java.util.Arrays;
 import java.util.List;
-
 import org.scijava.plugin.Plugin;
 import org.scijava.text.AbstractTextFormat;
 import org.scijava.text.TextFormat;
 
 /**
- * Text format for <a
- * href="http://daringfireball.net/projects/markdown/">Markdown</a>.
+ * Text format for
+ * <a href="http://daringfireball.net/projects/markdown/">Markdown</a>.
  * 
  * @author Curtis Rueden
  */
@@ -55,8 +55,11 @@ public class MarkdownTextFormat extends AbstractTextFormat {
 
 	@Override
 	public String asHTML(final String text) {
-		final MarkdownProcessor markdown = new MarkdownProcessor();
-		return markdown.markdown(text);
+		Parser parser = Parser.builder().build();
+		HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+		Node node = parser.parse(text);
+		return renderer.render(node);
 	}
 
 }
